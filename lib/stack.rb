dafab3c98c1b3b8b5f8ab337b889a87204bdd681
Kane Mathis <admin@kanemathis.com>
2023-02-09 17:11:06 -0500

    commit tree dd5d05e4722026544946ba17af3ae4eb89bcd25e

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,22 @@
 # your code here
+class Stack
+    def initialize
+        @stack=[]
+        self
+    end
+
+    def push(value)
+        @stack.push(value)
+        self
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[@stack.length-1]
+    end
+end
+
+s=Stack.new
